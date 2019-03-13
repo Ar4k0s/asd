522726f3decd9302ecb0d12f95420b7c8171262d
Ar4k0s <48204928+Ar4k0s@users.noreply.github.com>
2019-03-13 16:02:01 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
+
 const express = require("express")
 const mysql = require("mysql")
 const app = express()
 
-
+const table="vchody"
 
 var con = mysql.createConnection({
     host: "localhost",
     user: "jakub",
     password: "jakub",
-    database: "test",
+    database: "gov",
     multipleStatements: true
   });
   con.connect(function(err) {})
 
 
-app.use((req,res,next)=>{
+
+
+app.use('/',(req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin","*")
     res.setHeader("Content-Type","application/json")
     next()
 
 })
 app.get("/",function(req,res){
+    
     const query = req.query
+   
 
     const sqlInfo={
 
@@ -44,28 +49,32 @@ app.get("/",function(req,res){
             let a=[]
             b.forEach(x => a.push(x.data))
             this.columnsArr=a
-            a=a.toString()
-            this.columnsToSelect=a
+            this.columnsToSelect=a.join()
         }
     }
 
     sqlInfo.setColumnsToSelect(query.columns)
 
-    sql1=`SELECT ${sqlInfo.columnsToSelect} FROM gov WHERE districtname LIKE '${sqlInfo.search}%'  ORDER BY ${sqlInfo.columnsArr[sqlInfo.orderBy.column]} ${sqlInfo.orderBy.direction} LIMIT ${sqlInfo.len} OFFSET ${sqlInfo.start};`    
-    sql3 = "SELECT COUNT(*) as countAll FROM gov;"
-    sql2=`SELECT COUNT(*) as countFiltered FROM gov WHERE districtname LIKE '${sqlInfo.search}%';`    
+    sql1=`SELECT ${sqlInfo.columnsToSelect} FROM ${table} WHERE _id LIKE '${sqlInfo.search}%' AND _id > ${sqlInfo.start} ORDER BY ${sqlInfo.columnsArr[sqlInfo.orderBy.column]} ${sqlInfo.orderBy.direction} LIMIT ${sqlInfo.len} ;`    
+    //sql1=`SELECT ${sqlInfo.columnsToSelect} FROM ${table} WHERE _id LIKE '${sqlInfo.search}%' ORDER BY ${sqlInfo.columnsArr[sqlInfo.orderBy.column]} ${sqlInfo.orderBy.direction} LIMIT ${sqlInfo.len} OFFSET ${sqlInfo.start};`    
+    sql2=`SELECT MAX(_id) as countAll FROM ${table};`
+    sql3=`SELECT COUNT(*) as countFiltered   FROM ${table} WHERE _id LIKE '${sqlInfo.search}%';`    
+    
     
 
     sql = sql1+sql2+sql3
 
+   
+    
     con.query(sql, function (err, result) {
-        
+        console.log(result[0])
+        console.log('_____________________________')
         if(err) console.log(err); 
         else{               
-            console.log(result)
+            
             res.json({"draw":sqlInfo.draw,
-            "recordsTotal": result[2][0].countAll,
-            "recordsFiltered": result[1][0].countFiltered ,"data":result[0]})               
+            "recordsTotal": result[1][0].countAll,
+            "recordsFiltered": result[2][0].countFiltered ,"data":result[0]})               
           
         }
 
@@ -74,6 +83,12 @@ app.get("/",function(req,res){
 
    
 
+app.get("/txt", function(req,res){
+        
+        res.setHeader("Content-Type", "application/xls")
+        res.sendFile('/home/jakubbeno/Desktop/Pagination/api_key.xls')
+    })
+    
 
 
 app.listen(8000,function(){
